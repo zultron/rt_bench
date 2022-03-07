b68c96b25b9ec97a399362bffcc07722d9d9ebf6
John Morris <john@zultron.com>
2022-03-07 14:01:27 -0600

    apply_rt_tuning_configs.sh:  Parameterize & reorg
    - Add command line switches for everything
    - Split up things that used to be globbed together (e.g. cgroups pkgs
      & RT kernel; i915 kmodule & X windows)
    - Add nohz_full kernel cmdline option

--- apply_rt_tuning_configs.sh
+++ apply_rt_tuning_configs.sh
@@ -34,16 +34,13 @@ check_cpu() {
 
     case "$CPU" in
         "Intel(R) Celeron(R) CPU  N3160  @ 1.60GHz")
-            RT_CPUS=${RT_CPUS:-2,3}  # Shared L2 cache
-            GRUB_CMDLINE="isolcpus=${RT_CPUS}"
+            RT_CPUS=${RT_CPUS:-2-3}  # Shared L2 cache
             ;;
         "Intel(R) Core(TM) i5-8259U CPU @ 2.30GHz")
             RT_CPUS=${RT_CPUS:-3,7}  # Shared threads
-            GRUB_CMDLINE="isolcpus=${RT_CPUS}"
             ;;
         "Intel(R) Atom(TM) Processor E3950 @ 1.60GHz")
-            RT_CPUS=${RT_CPUS:-2,3}  # Shared L2 cache
-            GRUB_CMDLINE="isolcpus=${RT_CPUS}"
+            RT_CPUS=${RT_CPUS:-2-3}  # Shared L2 cache
             ;;
         *)
             echo "CPU '$CPU' unknown; please add to $BASH_SOURCE.  Exiting." >&2
@@ -51,6 +48,11 @@ check_cpu() {
             ;;
     esac
 
+    GRUB_CMDLINE=""
+    # Set isolcpus= if RT_CPUS is defined
+    test -z "$RT_CPUS" || GRUB_CMDLINE+=" isolcpus=$RT_CPUS"
+    # Set nohz_full= if RT_CPUS is defined
+    test -z "$RT_CPUS" || GRUB_CMDLINE+=" nohz_full=$RT_CPUS"
     # Mount cgroups v1 hierarchy for libcgroup (libcgroup v2.0 supports cgroups
     # v2, not in Bullseye)
     GRUB_CMDLINE+=" systemd.unified_cgroup_hierarchy=false"
@@ -85,17 +87,25 @@ check_cpu() {
 
 confirm_changes() {
     # Ask confirmation for we're about to do
-    cat >&2 <<-EOF
-		This script will:
-		- Install some utility packages
-		- Uninstall any Docker from Debian packages and install Docker CE
-		  - Add your user to the 'docker' group
-		- Configure kernel cmdline args '${GRUB_CMDLINE}'
-		- Install the RT kernel
-		- Disable Intel i915 graphics driver and XWindows
-
-	EOF
-    echo -n "WARNING:  Make these changes?  (y/N) " >&2
+    {
+        echo "This script will:"
+        test -z "${INSTALL_DEPS[*]}" || echo "- Install packages:  ${INSTALL_DEPS[*]}"
+        ! ${INSTALL_DOCKER:-false} || cat >&2 <<-EOF
+			- Uninstall any Docker from Debian packages and install Docker CE
+			  - Add your user to the 'docker' group
+			EOF
+        ! ${INSTALL_RT_KERNEL:-false} || echo "- Install the PREEMPT_RT kernel + header packages"
+        ! ${CONFIG_ISOLCPUS:-false} || echo "- Add isolcpus=${RT_CPUS} to kernel cmdline"
+        ! ${RM_CONFIG_ISOLCPUS:-false} || echo "- Remove isolcpus=${RT_CPUS} from kernel cmdline"
+        ! ${CONFIG_NOHZ_FULL:-false} || echo "- Add nohz_full=${RT_CPUS} to kernel cmdline"
+        ! ${RM_CONFIG_NOHZ_FULL:-false} || echo "- Remove nohz_full=${RT_CPUS} from kernel cmdline"
+        ! ${INSTALL_CGROUPS:-false} || echo "- Mount legacy cgroups at boot & install cgroup-tools"
+        ! ${DISABLE_GPU:-false} || echo "- Disable i915 graphics kernel module"
+        ! ${ENABLE_GPU:-false} || echo "- Reenable i915 graphics kernel module"
+        ! ${DISABLE_X:-false} || echo "- Disable X windows"
+        ! ${ENABLE_X:-false} || echo "- Reenable X windows"
+        echo -n "WARNING:  Make these changes?  (y/N) "
+    } >&2
     read REALLY
     if test ! "$REALLY" = y; then
         echo "Aborting script at user request" >&2
@@ -103,20 +113,24 @@ confirm_changes() {
     fi
 }
 
-confirm_remove() {
-    # Ask confirmation for we're about to do
-    cat >&2 <<-EOF
-		This script will:
-		- Remove any 'isolcpus=' kernel cmdline args
-		- Reenable the Intel i915 graphics driver and XWindows
-
-	EOF
-    echo -n "WARNING:  Make these changes?  (y/N) " >&2
-    read REALLY
-    if test ! "$REALLY" = y; then
-        echo "Aborting script at user request" >&2
-        exit 1
-    fi
+usage() {
+    cat 1>&2 <<-EOF
+		Usage:  $BASH_SOURCE [arg ...]
+		  -d:  Install Docker CE
+		  -k:  Install PREEMPT_RT kernel + header packages
+		  -i:  Configure 'isolcpus=' kernel command line option
+		  -I:  Remove 'isolcpus=' kernel command line option
+		  -z:  Configure 'nohz_full=' kernel command line option
+		  -Z:  Remove 'nohz_full=' kernel command line option
+		  -c:  Mount legacy cgroups at boot & install cgroup-tools
+		  -g:  Disable i915 Intel graphics kernel module
+		  -G:  Reenable i915 Intel graphics kernel module
+		  -x:  Disable X windows
+		  -X:  Reenable X windows
+		  -h:  Print this help message
+          $*
+		EOF
+    test -n "$*" && exit 0 || exit 1
 }
 
 # add_user_to_group group_name [user_name]
@@ -125,75 +139,6 @@ add_user_to_group() {
     local USER=${2:-$(id -un)}
     test $USER != 0 -a $USER != root || return
     ${SUDO} adduser $USER $GROUP
-}
-
-###########################
-# Install script deps
-###########################
-
-install_script_deps() {
-    ${APT_GET} install -y curl gnupg git
-}
-
-###########################
-# Install RT kernel
-# Configure isolated CPUs
-###########################
-
-install_rt_kernel() {
-    ${APT_GET} install -y linux-image-rt-amd64 linux-headers-rt-amd64
-    ${APT_GET} install -y cgroup-tools
-}
-
-config_isolcpus() {
-    GRUB_CMDLINE_CUR="$(source /etc/default/grub &&
-                           echo $GRUB_CMDLINE_LINUX_DEFAULT)"
-    if test -n "${GRUB_CMDLINE}" -a "$GRUB_CMDLINE_CUR" != "$GRUB_CMDLINE"; then
-        # Configure kernel cmdline args
-        ${SUDO} sed -i /etc/default/grub \
-            -e "s/.*\(GRUB_CMDLINE_LINUX_DEFAULT\).*/\1=\"${GRUB_CMDLINE}\"/"
-        ${SUDO} update-grub
-    fi
-}
-
-remove_isolcpus() {
-    GRUB_CMDLINE_CUR="$(source /etc/default/grub &&
-                           echo $GRUB_CMDLINE_LINUX_DEFAULT)"
-    shopt -s extglob  # Enable extended glob matching
-    GRUB_CMDLINE_NEW="${GRUB_CMDLINE_CUR/isolcpus=+([-0-9,])/}"
-
-    if test "$GRUB_CMDLINE_CUR" != "$GRUB_CMDLINE_NEW"; then
-        # Configure kernel cmdline args
-        ${SUDO} sed -i /etc/default/grub \
-            -e "s/.*\(GRUB_CMDLINE_LINUX_DEFAULT\).*/\1=\"${GRUB_CMDLINE_NEW}\"/"
-        ${SUDO} update-grub
-    fi
-}
-
-###########################
-# Disable i915 Intel graphics
-###########################
-
-disable_i915_graphics() {
-    # The i915 graphics driver and Intel HD Graphics are known killers of RT
-    # during heavy 3D load; disable the driver completely
-    ${SUDO} tee /etc/modprobe.d/blacklist-i915.conf <<-EOF
-		# Disable i915 graphics, RT performance killer
-		blacklist i915
-		# Other drivers may drag the i915 in from deps; prevent that
-		install i915 /usr/bin/false
-	EOF
-    ${SUDO} update-initramfs -u
-    # - Disable X
-    ${SUDO} ln -s /usr/lib/systemd/system/multi-user.target \
-        /etc/systemd/system/default.target
-}
-
-reenable_i915_graphics() {
-    ${SUDO} rm -f /etc/modprobe.d/blacklist-i915.conf
-    ${SUDO} update-initramfs -u
-    # - Reenable X
-    ${SUDO} rm -f /etc/systemd/system/default.target
 }
 
 ###########################
@@ -212,6 +157,7 @@ install_docker_ce() {
         ${APT_GET} remove -y \
             docker docker-engine docker.io || true
         # Add official Docker GPG key
+        ${APT_GET} install -y curl gnupg
         curl -fsSL https://download.docker.com/linux/${OS_VENDOR}/gpg |
             ${SUDO} apt-key add -
 
@@ -229,10 +175,128 @@ install_docker_ce() {
 }
 
 ###########################
+# Install RT kernel
+# Configure isolated CPUs
+###########################
+
+install_rt_kernel() {
+    ${APT_GET} install -y linux-image-rt-amd64 linux-headers-rt-amd64
+}
+
+config_isolcpus() {
+    GRUB_CMDLINE="$(source /etc/default/grub &&
+                        echo $GRUB_CMDLINE_LINUX_DEFAULT)"
+    GRUB_CMDLINE_TEST="$(echo $GRUB_CMDLINE | sed 's/isolcpus=//')"
+    if test "$GRUB_CMDLINE" != "$GRUB_CMDLINE_TEST"; then
+        echo "Warning:  grub command line already has isolcpus=; doing nothing" 1>&2
+        return
+    fi
+    GRUB_CMDLINE+=" isolcpus=$RT_CPUS"
+    ${SUDO} sed -i /etc/default/grub \
+        -e "s/.*\(GRUB_CMDLINE_LINUX_DEFAULT\).*/\1=\"${GRUB_CMDLINE}\"/"
+    UPDATE_GRUB=true
+}
+
+remove_isolcpus() {
+    GRUB_CMDLINE_CUR="$(source /etc/default/grub &&
+                           echo $GRUB_CMDLINE_LINUX_DEFAULT)"
+    shopt -s extglob  # Enable extended glob matching
+    GRUB_CMDLINE="${GRUB_CMDLINE_CUR/isolcpus=+([-0-9, ])/}"
+
+    if test "$GRUB_CMDLINE_CUR" != "$GRUB_CMDLINE"; then
+        # Configure kernel cmdline args
+        ${SUDO} sed -i /etc/default/grub \
+            -e "s/.*\(GRUB_CMDLINE_LINUX_DEFAULT\).*/\1=\"${GRUB_CMDLINE}\"/"
+        UPDATE_GRUB=true
+    fi
+}
+
+config_nohz_full() {
+    GRUB_CMDLINE="$(source /etc/default/grub &&
+                        echo $GRUB_CMDLINE_LINUX_DEFAULT)"
+    GRUB_CMDLINE_TEST="$(echo "$GRUB_CMDLINE" | sed 's/nohz_full=//')"
+    if test "$GRUB_CMDLINE" != "$GRUB_CMDLINE_TEST"; then
+        echo "Warning:  grub command line already has nohz_full=; doing nothing" 1>&2
+        return
+    fi
+    GRUB_CMDLINE+=" nohz_full=$RT_CPUS"
+    ${SUDO} sed -i /etc/default/grub \
+        -e "s/.*\(GRUB_CMDLINE_LINUX_DEFAULT\).*/\1=\"${GRUB_CMDLINE}\"/"
+    UPDATE_GRUB=true
+}
+
+remove_nohz_full() {
+    GRUB_CMDLINE_CUR="$(source /etc/default/grub &&
+                           echo $GRUB_CMDLINE_LINUX_DEFAULT)"
+    shopt -s extglob  # Enable extended glob matching
+    GRUB_CMDLINE="${GRUB_CMDLINE_CUR/nohz_full=+([-0-9, ])/}"
+
+    if test "$GRUB_CMDLINE_CUR" != "$GRUB_CMDLINE"; then
+        # Configure kernel cmdline args
+        ${SUDO} sed -i /etc/default/grub \
+            -e "s/.*\(GRUB_CMDLINE_LINUX_DEFAULT\).*/\1=\"${GRUB_CMDLINE}\"/"
+        UPDATE_GRUB=true
+    fi
+}
+
+install_cgroups() {
+    # Install tools
+    ${SUDO} apt-get install -y cgroup-tools
+    # Mount cgroups v1 hierarchy for libcgroup (libcgroup v2.0 supports cgroups
+    # v2, not in Bullseye)
+    GRUB_CMDLINE="$(source /etc/default/grub &&
+                        echo $GRUB_CMDLINE_LINUX_DEFAULT)"
+    if $(echo $GRUB_CMDLINE | grep -q 'unified_cgroup_hierarchy'); then
+        echo "Warning:  cgroups already enabled in kernel cmdline" 1>&2
+        return
+    else
+        GRUB_CMDLINE+=" systemd.unified_cgroup_hierarchy=false"
+        GRUB_CMDLINE+=" systemd.legacy_systemd_cgroup_controller=false"
+        ${SUDO} sed -i /etc/default/grub \
+            -e "s/.*\(GRUB_CMDLINE_LINUX_DEFAULT\).*/\1=\"${GRUB_CMDLINE}\"/"
+        UPDATE_GRUB=true
+    fi
+}
+
+###########################
+# Disable graphics
+###########################
+
+disable_i915_graphics() {
+    # The i915 graphics driver and Intel HD Graphics are known killers of RT
+    # during heavy 3D load; disable the driver completely
+    ${SUDO} tee /etc/modprobe.d/blacklist-i915.conf <<-EOF
+		# Disable i915 graphics, RT performance killer
+		blacklist i915
+		# Other drivers may drag the i915 in from deps; prevent that
+		install i915 /usr/bin/false
+	EOF
+    ${SUDO} update-initramfs -u
+}
+
+reenable_i915_graphics() {
+    ${SUDO} rm -f /etc/modprobe.d/blacklist-i915.conf
+    ${SUDO} update-initramfs -u
+}
+
+disable_x() {
+    ${SUDO} ln -s /usr/lib/systemd/system/multi-user.target \
+        /etc/systemd/system/default.target
+}
+
+reenable_x() {
+    ${SUDO} rm -f /etc/systemd/system/default.target
+}
+
+
+###########################
 # Finish up
 ###########################
 
 finalize() {
+    # Run update-grub, if needed
+    ! $UPDATE_GRUB || ${SUDO} update-grub
+
     set +x
     cat >&2 <<-EOF
 
@@ -246,38 +310,43 @@ finalize() {
 
 
 ###########################
-# Install everything
+# Execute command line args
 ###########################
 
-install_or_remove() {
-    case "$1" in
-        remove) remove_everything ;;
-        install) install_everything ;;
-        *) echo "Usage:  $BASH_SOURCE [install|remove]" >&2; exit 1 ;;
-    esac
-}
+if $CALLED_AS_SCRIPT; then
+    while getopts :dkiIzZgGxXch ARG; do
+        case $ARG in
+            d) INSTALL_DOCKER=true ;;
+            k) INSTALL_RT_KERNEL=true; APT_GET_UPDATE=true ;;
+            i) CONFIG_ISOLCPUS=true; CHECK_CPU=true ;;
+            I) RM_CONFIG_ISOLCPUS=true ;;
+            z) CONFIG_NOHZ_FULL=true; CHECK_CPU=true ;;
+            Z) RM_CONFIG_NOHZ_FULL=true ;;
+            c) INSTALL_CGROUPS=true; APT_GET_UPDATE=true ;;
+            g) DISABLE_GPU=true ;;
+            G) ENABLE_GPU=true ;;
+            x) DISABLE_X=true ;;
+            X) ENABLE_X=true ;;
+            h) usage ;;
+            *) usage "Unknown option '-$ARG'" ;;
+        esac
+    done
+    shift $(($OPTIND - 1))
 
-remove_everything() {
-    confirm_remove
-    # At this point we're committed; show what we're doing
-    set -x
-    remove_isolcpus
-    reenable_i915_graphics
-}
-
-install_everything() {
-    check_cpu
+    ! ${CHECK_CPU:-false} || check_cpu
     confirm_changes
-
-    # At this point we're committed; show what we're doing
-    set -x
-    install_script_deps
-    install_docker_ce
-    install_rt_kernel
-    config_isolcpus
-    disable_i915_graphics
+    UPDATE_GRUB=false
+    ! ${APT_GET_UPDATE:-false} || ${APT_GET} update
+    ! ${INSTALL_DOCKER:-false} || install_docker_ce
+    ! ${INSTALL_RT_KERNEL:-false} || install_rt_kernel
+    ! ${CONFIG_ISOLCPUS:-false} || config_isolcpus
+    ! ${RM_CONFIG_ISOLCPUS:-false} || remove_isolcpus
+    ! ${CONFIG_NOHZ_FULL:-false} || config_nohz_full
+    ! ${RM_CONFIG_NOHZ_FULL:-false} || remove_nohz_full
+    ! ${INSTALL_CGROUPS:-false} || install_cgroups
+    ! ${DISABLE_GPU:-false} || disable_i915_graphics
+    ! ${ENABLE_GPU:-false} || reenable_i915_graphics
+    ! ${DISABLE_X:-false} || disable_x
+    ! ${ENABLE_X:-false} || reenable_x
     finalize
-}
-
-# Install everything if called as a script
-! $CALLED_AS_SCRIPT || install_or_remove "$@"
+fi
